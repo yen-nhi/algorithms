ebde42aacfb6fab494d9b0dbee52f0b8ee16765b
Yen Nhi <yenhinguyen.92@gmail.com>
2020-03-22 09:50:31 +0700

    Max Level Sum in Binary Tree.py

--- geeksforgeeks/Max Level Sum in Binary Tree.py
+++ geeksforgeeks/Max Level Sum in Binary Tree.py
@@ -10,23 +10,14 @@ class Node:
 def sum_each_level(root, h, heights):
     if root is None:
         return
-    if heights[h] is None:
-        heights[h] = root.data
+    if len(heights) <= h:
+        heights.append(root.data)
     else:
         heights[h] += root.data
     sum_each_level(root.left, h + 1, heights)
     sum_each_level(root.right, h + 1, heights)
-
-def find_max(heights):
-    i = 0
-    max_int = float("-inf")
-    while heights[i] is not None:
-        if heights[i] > max_int:
-            max_int = heights[i]
-        i += 1
-    return max_int
-
+    
 def maxLevelSum(root):
-    heights = [None] * 10000
+    heights = []
     sum_each_level(root, 0, heights)
-    return find_max(heights)
+    return max(heights)
